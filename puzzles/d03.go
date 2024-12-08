3b9eb347e6ce2decbdd79013da1f6b8b48d5b168
amphipath <amphipath@users.noreply.github.com>
2024-12-08 10:34:54 +0800

    finish d3p2

--- puzzles/d03.go
+++ puzzles/d03.go
@@ -1,10 +1,12 @@
 package puzzles
 
 import (
+	"fmt"
 	"regexp"
 	"strconv"
 
 	"github.com/tobby-s/advent-of-code-2024/utils"
+
 )
 
 func D3P1() int {
@@ -24,5 +26,27 @@ func D3P1() int {
 }
 
 func D3P2() int {
-	return 0
+	url := "https://adventofcode.com/2024/day/3/input"
+	data := utils.LoadData(url)
+	regex := regexp.MustCompile(`(mul\((\d+),(\d+)\)|do\(\)|don't\(\))`)
+	sum := 0
+	enabled := true
+	for _, instructions := range data {
+		matches := regex.FindAllStringSubmatch(instructions,-1)
+		for _, m := range matches {
+			switch {
+			case m[0] == "do()":
+				enabled = true
+			case m[0] == "don't()":
+				enabled = false
+			case len(m) >= 4:
+				num1, _ := strconv.Atoi(m[2])
+				num2, _ := strconv.Atoi(m[3])
+				if enabled {
+					sum += num1 * num2
+				}
+			}
+		}
+	}
+	return sum
 }
